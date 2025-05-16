b8b5f981f36f40a6946982d8c13432fa453ca420
mark <mmackenzie.uk@gmail.com>
2025-05-16 12:44:50 +0100

    wip

--- app/actions/get-actions.ts
+++ app/actions/get-actions.ts
@@ -1,33 +1,18 @@
 "use server"
 
-
 import { fetchWrapper } from "../ts/fetchWrapper";
 
+export const getCategories = async () => await fetchWrapper.get(`categories`);
 
-export async function getCategories() {
-    const resp = await fetchWrapper.get(`categories`);
-    return resp;
-}
+export const findAll = async (currentPage: number, ITEMS_PER_PAGE: number) => 
+    await fetchWrapper.get(`products/findAll/${currentPage}/${ITEMS_PER_PAGE}`);
 
-export async function findAll(currentPage: number, ITEMS_PER_PAGE: number) {
-    const resp = await fetchWrapper.get(`products/findAll/${currentPage}/${ITEMS_PER_PAGE}`);
-    return resp;
-}
+export const findByCategory = async (category: string, currentPage: number, ITEMS_PER_PAGE: number) => 
+    await fetchWrapper.get(`products/findByCategory/${category}/${currentPage}/${ITEMS_PER_PAGE}`);
 
-export async function findByCategory(category: string, currentPage: number, ITEMS_PER_PAGE: number) {
-    const resp = await fetchWrapper.get(`products/findByCategory/${category}/${currentPage}/${ITEMS_PER_PAGE}`);
-    return resp;
-}
-
-export async function getProductPageData(slug : string) {
-
-    const resp = await fetchWrapper.get(`products/getProductPage/${slug}`);
-
-    console.log("resp -prod page: ", resp)
-
-    return resp;
-}
-
+export const getProductPageData = async (slug : string) => 
+    await fetchWrapper.get(`products/getProductPage/${slug}`);
+   
 
 // export async function getCount(ITEMS_PER_PAGE: number) {
 //     const count = productsService.count(ITEMS_PER_PAGE)
