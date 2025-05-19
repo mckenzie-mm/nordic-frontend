033ccda9a3d7a83d355310ed5c94348b7bb5815d
mark <mmackenzie.uk@gmail.com>
2025-05-19 20:49:28 +0100

    use fetch

--- app/actions/products.ts
+++ app/actions/products.ts
@@ -1,8 +1,16 @@
 "use server"
 import { fetchWrapper } from "../ts/fetchWrapper";
 
-export const findAll = async (currentPage: number, ITEMS_PER_PAGE: number) => 
-    await fetchWrapper.get(`products/list/${currentPage}/${ITEMS_PER_PAGE}`);
+export const findAll = async (currentPage: number, ITEMS_PER_PAGE: number) => {
+    const response = await fetch(`http://localhost:5037/products/list/${currentPage}/${ITEMS_PER_PAGE}`);
+
+    console.log("response ", response);
+
+    const data = await response.json();
+
+    console.log("data ", data)
+}
+
 
 export const findByCategory = async (category: string, currentPage: number, ITEMS_PER_PAGE: number) => 
     await fetchWrapper.get(`products/list/${category}/${currentPage}/${ITEMS_PER_PAGE}`);
