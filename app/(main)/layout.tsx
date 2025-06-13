479aceeefd40fa34e4bb42e5db2652ddc35653f3
mark <mmackenzie.uk@gmail.com>
2025-06-14 06:03:06 +1000

    improve image loading speed

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,7 +7,12 @@ export default function RootLayout({
 }>) {
   return (
     <>
-        <Carousel />
+        <div className="container-bak">
+          <img src={'/wwurm/img-1-thb.jpg'} alt="carousel image"  style={{width: "100%"}} />
+          <div style={{position: "absolute", top: 0, left: 0, width: "100%" }}>
+            <Carousel />
+          </div>     
+        </div>
         {children}
     </>
   )
